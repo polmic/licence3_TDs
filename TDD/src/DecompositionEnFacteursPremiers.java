47d09e9cb5a3ad4f6f65325ca02f356421a4ebd5
Paul <polmic31@gmail.com>
2017-11-14 17:47:19 +0100

    Auto stash before rebase of "origin/master"

--- TDD/src/DecompositionEnFacteursPremiers.java
+++ TDD/src/DecompositionEnFacteursPremiers.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class DecompositionEnFacteursPremiers {
   List<Integer> factors;
 
-  public List<Integer> compute(int n) {
+  public List<Integer> computeWhile(int n) {
     factors = new ArrayList<>();
     int facteurPremier = 2;
     while (n > 1) {
@@ -17,4 +17,15 @@ public class DecompositionEnFacteursPremiers {
     return factors;
   }
 
+  public List<Integer> compute(int n) {
+    factors = new ArrayList<>();
+    for (int facteurPremier = 2 ; n > 1 ; facteurPremier++)
+      while (n % facteurPremier == 0) {
+        factors.add(facteurPremier);
+        n /= facteurPremier;
+    }
+    return factors;
+  }
+
 }
+
